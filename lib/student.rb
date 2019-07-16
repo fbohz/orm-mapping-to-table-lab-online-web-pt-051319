87f124b1d2b8dc12c51a4bb1406d7d1d134c255a
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-07-16 20:35:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,16 +15,14 @@ class Student
      id INTEGER PRIMARY KEY,
      name TEXT,
      grade TEXT
-     )
+     );
      SQL
     DB[:conn].execute(sql)
   end 
   
   def self.drop_table
     sql = <<-SQL
-     
-    
-    
+     DROP TABLE students; 
     SQL
     
     DB[:conn].execute(sql)
