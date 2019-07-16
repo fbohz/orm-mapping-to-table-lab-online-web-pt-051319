ecf7c72800d38dad1e3883df82ce0ebf4ae9ef38
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-07-16 20:28:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,10 +11,10 @@ class Student
   
   def self.create_table 
    sql =  <<-SQL
-    CREATE TABLE IF NOT EXISTS students (
+    CREATE TABLE IF NOT EXISTS songs (
      id INTEGER PRIMARY KEY,
      name TEXT,
-     grade TEXT
+     album TEXT
      )
     SQL 
    
